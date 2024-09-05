32b139188843e93a60431a5072318c163874a1e5
Polyák Zalán <polyakzalan394@gmail.com>
2024-09-05 09:41:21 +0200

    0905

---
+++ Solution/Vehicles.Database/Entities/VehicleEntity.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Vehicles.Database.Entities
+{
+    [Table("vehicle")]
+    public class VehicleEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public uint Id { get; set; }
+
+        [Required]
+        [StringLength(7)]
+        public string LicensePlate { get; set; }
+
+        [Required]
+        [StringLength(17)]
+        public string ChassisNumber { get; set; }
+
+        [Required]
+        [StringLength(2)]
+        public string EngineNumber { get; set; }
+
+        [Required]
+        [Range(3, 7)]
+        public uint numOfDoors { get; set; }
+
+        [Required]
+        public uint Weight { get; set; }
+
+        [Required]
+        public uint Power { get; set; }
+    }
+}
